c573590d565755465c180408af49a84a81b965be
Siddhartha Pothukuchi <siddharth.pothukuchi@gmail.com>
2024-12-30 12:44:00 -0500

    final

---
+++ main.py
@@ -0,0 +1,32 @@
+import random 
+
+# blackjack game
+
+player = 0
+comp = 0 
+
+while True:
+    if comp > 21:
+        print("Player automatically wins, the computer bust!")
+        break
+    if player > 21:
+        print("Computer automatically wins, the player bust!")
+        break
+    hit = input("Would you like to hit? Y or N: ")
+    crand = random.randint(1,11)
+    comp = comp + crand
+   
+    if hit == "Y":
+        rand = random.randint(1,11)
+        player = player + rand
+        print("You got " + str(rand))
+        print("The computer is currently at " + str(comp))
+        print("You're at " + str(player))
+    else:
+        print("You're at " + str(player) + " while the computer is at " + str(comp))
+        if comp > player:
+            print("Computer Wins!")
+        elif player > comp:
+            print("Player Wins!")
+        else:
+            print("It's a tie, play another rouund!")
